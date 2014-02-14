677f7bc462cc520f27365f08b893bfa49f2ef9d5
m1093782566 <dj199008@gmail.com>
2014-02-14 15:37:11 +0800

    wait_subprocess_and_SIGCLD.rb

---
+++ Fibers_Threads_Processes/wait_subprocess_and_SIGCLD.rb
@@ -0,0 +1,18 @@
+exec("sort testfile > output.txt") if fork.nil?
+# The sort is now running in a child process
+# carry on processing in the main program
+# ... dum di dum ...
+# then wait for the sort to finish, mainprocess do nothing but wait!
+Process.wait
+
+#If you'd rather be notified when a child exits (instead of just waiting around),
+#you can set up a signal handler using Object#trap
+trap("CLD") do #register the process block for signal CLD
+  pid = Process.wait
+  puts "Child pid #{pid}: terminated"
+end
+
+fork { exec("sort testfile > output.txt") } #subprocess execute it firstly, when finished, it will call registered block
+# Do other stuff...
+# produces:
+# Child pid 22026: terminated
