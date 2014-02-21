11a9c7a3ca17d82d9eaea8d9a0deef77f26c4407
m1093782566 <dj199008@gmail.com>
2014-02-21 16:21:49 +0800

    Dir[]_Dir.glob.rb

---
+++ File_operate/Dir[]_Dir.glob.rb
@@ -0,0 +1,6 @@
+# Removes all symlinks from blobs directory
+def remove_symlinks #Dir[] is equivalent to Dir.glob()
+  Dir[File.join(@blobs_dir, "**", "*")].each do |file| #** means recursive, * means any file.
+    FileUtils.rm_rf(file) if File.symlink?(file)
+  end
+end
