7977af03e685fc949a5c7960ff25c13c1ee55660
m1093782566 <dj199008@gmail.com>
2014-04-25 16:46:49 +0800

    yaml.rb

--- yaml.rb
+++ yaml.rb
@@ -1,15 +1,20 @@
-def load_config(file)
-  if File.exists?(file)
-    load_yaml_file(file)
-  else
-    {}
-  end
-end
-
 def write_yaml(file, hash)
   unless hash == load_config(file)
     File.open(file, "w+") do |f|
       Psych.dump(hash, f)
     end
   end
+end
+
+def load_yaml_file(path, expected_type = Hash)
+  raise(ConfigError, "Cannot find file `#{path}'") unless File.exists?(path)
+  yaml = Psych.load_file(path)
+
+  if expected_type && !yaml.is_a?(expected_type)
+    raise ConfigError, "Incorrect file format in `#{path}', #{expected_type} expected"
+  end
+
+  yaml
+rescue SystemCallError => e
+  raise ConfigError, "Cannot load YAML file at `#{path}': #{e}"
 end
