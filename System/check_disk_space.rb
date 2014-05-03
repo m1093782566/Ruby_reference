27d27c99b3a1dda043a4338336d9a68dab7f9f03
DuJun <m1093782566@163.com>
2014-05-03 14:59:05 +0800

    Update check_disk_space.rb

--- System/check_disk_space.rb
+++ System/check_disk_space.rb
@@ -1,2 +1,13 @@
 require 'sys/filesystem'
 include Sys
+
+#check if space of a dir is enough
+def check_available_disk_space(dir, size)
+  begin
+    stat = Sys::Filesystem.stat(dir)
+    available_space = stat.block_size * stat.blocks_available
+    available_space > size ? true : false
+  rescue
+    false
+  end
+end
