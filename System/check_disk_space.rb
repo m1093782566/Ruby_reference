c7d825c3ef9eaf7ebcef221e5f1bae804b05c1cd
DuJun <m1093782566@163.com>
2014-05-03 15:00:11 +0800

    Update check_disk_space.rb

--- System/check_disk_space.rb
+++ System/check_disk_space.rb
@@ -2,6 +2,7 @@ require 'sys/filesystem'
 include Sys
 
 #check if space of a dir is enough
+#bosh/bosh-director/lib/bosh/director/api/api_helper.rb
 def check_available_disk_space(dir, size)
   begin
     stat = Sys::Filesystem.stat(dir)
