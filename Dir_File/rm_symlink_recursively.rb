9116fa220e1d4d2db78e1d3bba13dcef16ba4a19
DuJun <m1093782566@163.com>
2014-05-13 20:56:49 +0800

    Update rm_symlink_recursively.rb

--- Dir_File/rm_symlink_recursively.rb
+++ Dir_File/rm_symlink_recursively.rb
@@ -1,3 +1,4 @@
+# Reference https://github.com/cloudfoundry/bosh/blob/master/bosh_agent/lib/bosh_agent/message/apply.rb#L54
 # Remove all symlink targets
 Dir.glob(File.join(dir, "*")).each do |f|
   if File.symlink?(f)
