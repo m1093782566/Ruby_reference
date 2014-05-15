971b0e652839428ca1b88d45c3f3a805252cc6df
DuJun <m1093782566@163.com>
2014-05-15 20:43:46 +0800

    Update restart_dhclient.rb

--- System/restart_dhclient.rb
+++ System/restart_dhclient.rb
@@ -1,3 +1,4 @@
+#Reference https://github.com/cloudfoundry/bosh/blob/master/bosh_agent/lib/bosh_agent/platform/ubuntu/network.rb#L62
 def restart_dhclient
   sh("pkill dhclient3", :on_error => :return)
   sh("/etc/init.d/networking restart", :on_error => :return)
